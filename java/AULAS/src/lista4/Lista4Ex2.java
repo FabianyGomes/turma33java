b4a5d93bd579edef0a91ab188760cc7dfad347e0
FabianyGomes <gomesfabiany@hotmail.com>
2021-09-03 17:10:16 -0300

    AULA JAVA 03/09/2021 E EXERCÍCIOS LISTA 4

--- java/AULAS/src/lista4/Lista4Ex2.java
+++ java/AULAS/src/lista4/Lista4Ex2.java
@@ -1,5 +1,33 @@
 package lista4;
 
+import java.util.Scanner;
+
 public class Lista4Ex2 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int[][] matriz = new int[3][3];
+
+		int linha, coluna, somaTotal = 0, SomaDiagonal = 0;
+
+		for (linha = 0; linha < 3; linha++) {
+			for (coluna = 0; coluna < 3; coluna++) {
+				System.out.print("Digite o vetor da matriz na linha" + linha + " e na coluna " + coluna + " : ");
+				matriz[linha][coluna] = in.nextInt();
+				somaTotal = somaTotal + matriz[linha][coluna];
+
+			}
+		}
+
+		for (linha = 0; linha < 3; linha++) {
+			for (coluna = 0; coluna < 3; coluna++) {
+				System.out.print("[ " + matriz[linha][coluna] + " ]");
+			}
+			System.out.println("\n");
+		}
+		SomaDiagonal = ((matriz[0][0]) + (matriz[1][1]) + (matriz[2][2]));
+		System.out.println("\n A soma total é de: " + somaTotal);
+		System.out.println("\n A soma da diagonal principal é de: " + SomaDiagonal + "\n");
+	}
 
 }
